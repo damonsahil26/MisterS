a8019e10252e487e152c759be4592815b3365b88
Sahil Malhotra <sahil061626@gmail.com>
2022-09-24 22:10:23 +0530

    Added Duende Identity and nuget packages.

---
+++ MisterS.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MisterS.Services.Identity.Models;
+
+namespace MisterS.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
